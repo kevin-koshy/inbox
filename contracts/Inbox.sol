8166631244312d565f51a4a0a335b55d0c5a9c14
kevin-koshy <84186576+kevin-koshy@users.noreply.github.com>
2022-03-28 18:40:59 +0530

    Second Commmit

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -2,3 +2,28 @@ pragma solidity ^0.4.17;
 // linter warnings (red underline) about pragma version can igonored!
 
 // contract code will go here
+pragma solidity ^0.4.17;
+
+contract Inbox {
+    string public message;
+
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+
+     function getMessage() public view returns (string){
+         return message;
+     }
+
+    function doMath(int a, int b){
+        a + b;
+        b - a;
+        a * b;
+        a == 0;
+    }
+
+}
