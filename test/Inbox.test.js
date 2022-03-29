87a33207d47597dd54546eb29552bba7c36109ad
kevin-koshy <84186576+kevin-koshy@users.noreply.github.com>
2022-03-29 16:59:27 +0530

    adding mocha test

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -4,4 +4,41 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3'); // constructor function
 const web3  = new Web3(ganache.provider());
 
+//
+// class Car {
+//     park() {
+//         return 'stopped';
+//     }
+//     drive() {
+//         return 'vroom';
+//     }
+// }
+//
+// let car;
+//
+// beforeEach(()=> {
+//      car = new Car();
+// });
+// describe('Car Class', ()=>{
+//     it('can park', ()=> {
+//         assert.equal(car.park(), 'stopped');
+//     });
+//     it('can drive', ()=>{
+//         assert.equal(car.drive(), 'vroom');
+//     });
+// });
 
+beforeEach(() =>{
+    //Get a list of all accounts
+    web3.eth.getAccounts()
+        .then(fetchedAccounts => {
+            console.log(fetchedAccounts);
+        })
+
+
+});
+describe('Inbox', () => {
+    it('deploys a contract', () => {
+
+    });
+});
