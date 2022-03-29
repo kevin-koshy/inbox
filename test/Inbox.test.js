2413fd35354457b458655a1b5839bdb79a6523f3
kevin-koshy <84186576+kevin-koshy@users.noreply.github.com>
2022-03-29 17:29:59 +0530

    adding test for inbox - phase 1

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -3,6 +3,7 @@ const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); // constructor function
 const web3  = new Web3(ganache.provider());
+const {interface, bytecode} = require('../compile');
 
 //
 // class Car {
@@ -28,17 +29,20 @@ const web3  = new Web3(ganache.provider());
 //     });
 // });
 
-beforeEach(() =>{
+let accounts;
+let inbox;
+
+beforeEach(async () =>{
     //Get a list of all accounts
-    web3.eth.getAccounts()
-        .then(fetchedAccounts => {
-            console.log(fetchedAccounts);
-        })
-
-
+    accounts = await web3.eth.getAccounts()
+    // Use one of the accounts to deploy the contract
+    inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({data:bytecode, arguments:['Hi there!'] })
+        .send({from: accounts[0], gas:'1000000'})
 });
+
 describe('Inbox', () => {
     it('deploys a contract', () => {
-
+    console.log(inbox);
     });
 });
