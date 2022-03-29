dc9283485ff33ab63f7bc0fa03d634210eb7ecf4
kevin-koshy <84186576+kevin-koshy@users.noreply.github.com>
2022-03-29 12:17:59 +0530

    Third Commit

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,7 @@
 // contract test code will go here
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3'); // constructor function
+const web3  = new Web3(ganache.provider());
+
+
